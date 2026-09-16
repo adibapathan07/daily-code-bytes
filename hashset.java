caa554071875aefb16d170d1e0e1b6b1445287d4
adibapathan07 <adibapathan248@gmail.com>
2026-09-16 05:32:29 -0700

    (added hashset.java file)

--- hashset.java
+++ hashset.java
@@ -5,12 +5,12 @@ public class hashset {
         set.add(1);
         set.add(2);
         set.add(1);
-        
+         System.out.println(" size of set is :" + set.size());
+         System.out.println(set);
 
-        if (set.contains(1));
-        System.out.println("set contains 1");
-        if (!set.contains(6));
-        System.out.println("does not contain");
+         Iterator it = set.iterator();
+         System.out.println(it.next());
 
+       
     }
 }
