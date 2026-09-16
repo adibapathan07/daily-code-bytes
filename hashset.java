e75cb62ec43cc0e5060dc33428d29e9469b985e1
adibapathan07 <adibapathan248@gmail.com>
2026-09-16 05:15:16 -0700

    maine new code complete kiya

---
+++ hashset.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class hashset {
+    public static void main(String [] args){//
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(1);
+        
+
+        if (set.contains(1));
+        System.out.println("set contains 1");
+        if (!set.contains(6));
+        System.out.println("does not contain");
+
+    }
+}
