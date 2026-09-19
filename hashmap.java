c0515fc1c153c8df8d6938983ae29242752551e2
adibapathan07 <adibapathan248@gmail.com>
2026-09-19 06:42:14 -0700

    implement key-values pairs storage using HashMap

--- hashmap.java
+++ hashmap.java
@@ -1,3 +1,17 @@
+import java.util.HashMap;
+
 public class hashmap {
+    public static void main (String []args){
+        HashMap<String,Integer> map = new HashMap<>();
+
+        
+        map.put("US", 30);
+        map.put("India", 120);
+        map.put("China", 30);
+
+
+        System.out.println(map);
+
+    }
     
 }
